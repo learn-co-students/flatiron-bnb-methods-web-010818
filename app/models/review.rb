0a524e55a6cc0dc28dcce82b96ed79a55f0e4382
katycarr <khcarr9@gmail.com>
2018-02-06 16:24:06 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, :description, :reservation_id, presence: true
+
+  validate :reservation_in_past
+
+  # validations
+
+  def reservation_in_past
+    if reservation && reservation.checkout.to_date > Date.today
+      errors.add(:base, "Checkout date must be in the past")
+    end
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
+  has_many :guests, through: :reservations, class_name: "User", foreign_key: 'host_id'
+
+
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+  def host_reviews
+    Review.all.select do |review|
+      review.reservation.listing.host = self
+    end
+  end
+
+  def hosts
+    User.all.select do |user|
+      user.guests.include?(self)
+    end
+  end
 end
